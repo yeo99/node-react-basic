571164e4d70d868e2a420c08ef3d9e57a9a315e5
qw6375@gmail.com <timek70@dalcomsoft.com>
2022-05-23 18:48:52 +0900

    로그인, 로그아웃 구현

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -17,14 +17,15 @@ app.use(cookieParser());
 // mongodb+srv://timek70:<password>@boilerplate.jifwa.mongodb.net/?retryWrites=true&w=majority
 const mongoose = require('mongoose');
 const { json } = require('express/lib/response');
+const req = require('express/lib/request');
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/register', (req, res) => {
+app.post('api/users/register', (req, res) => {
     // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 DB에 넣어줌
 
     const user = new User(req.body)
@@ -38,7 +39,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일이 데이터베이스에 있는지 찾는다
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -49,10 +50,10 @@ app.post('/login', (req, res) => {
         }
         // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
         user.comparePassword(req.body.password, (err, isMatch) => {
-            // true로 전달되어야할 isMatch가 false라면 
-            if (!isMatch)
+            // true로 전달되어야할 isMatch가 false라면
+            if (!isMatch) {
                 return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
-
+            }
             // 비밀번호까지 맞다면 토큰을 생성
             user.generateToken((err, user) => {
                 if(err) return res.status(400).send(err);
@@ -66,6 +67,33 @@ app.post('/login', (req, res) => {
 
     })
 
+})
+
+// auth 미들웨어 추가. endpoint의 request를 받은 다음 callback하기전에 중간에서 동작함. /middleware/auth.js에서 처리
+app.get('/api/users/auth', auth, (req, res) => {
+    // 여기 까지 미들웨어를 통과해 왔다는 것은 Authentication이 true
+    req.status(200).json({
+        // 미들웨어에서 auth성공 시 req에 user를 넣어서 보냈으므로 사용 가능
+        _id: req.user.id,
+        // role: 0 => 일반 유저, role: 1 => 관리자
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+// 로그아웃 기능. login된 상태에서 행위이므로 auth미들웨어도 넣어줌
+// 로그아웃 Route -> 로그아웃 하려는 유저를 DB에서 찾아서 -> 해당 유저의 토큰을 지워줌
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id}, {token: ""}, (err, user) => {
+        if(err) return res.json({succes: false, err})
+        return res.status(200).send({
+            succes: true
+        })
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;  // 10자리인 salt를 생성하여 암호화에 이용
 const jwt = require('jsonwebtoken');
+const { decode } = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -81,6 +82,21 @@ userSchema.methods.generateToken = function(cb) {
     user.save(function(err, user) {
         if(err) return cb(err)
         cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id" : decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
     })
 }
 
