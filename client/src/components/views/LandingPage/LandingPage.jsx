2194c1a49b73d11e21db1d14344bb264db9bfb50
qw6375@gmail.com <timek70@dalcomsoft.com>
2022-05-24 14:37:17 +0900

    라우팅 처리 전 컴포넌트 생성

---
+++ client/src/components/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+// 첫 화면을 위한 jsx파일. ES7+ React/Redux/React-Native snippets  v4.4.3 (dsznajder) Extension을 이용하면 편하다
+// rfce + 'TAB' 입력시 function component(export)가 생성되고, rce + 'TAB' 입력시 class component(export)가 생성된다.
+// e빼면 export없이 생성
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage
