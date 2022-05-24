171181870dac33762567704ac9e94e89274a8973
qw6375@gmail.com <timek70@dalcomsoft.com>
2022-05-24 18:39:29 +0900

    Redux, Redux-thunk, Redux-promise 추가

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// App을 Provider를 이용하여 Redux에 연결해줘야함
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleWare from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducer';
 
+// 그냥 store는 객체밖에 못받기 때문에, promise와 function도 받을 수 있도록 함
+const createStoreWithMiddleWare = applyMiddleware(promiseMiddleWare, ReduxThunk)(createStore)
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleWare(Reducer,
+        // Redux Dev Tools Extension 사용 가능하도록
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
