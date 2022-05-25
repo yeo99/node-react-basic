8ec2e270da5dc35891fd92cbf376a7d8c5e22499
qw6375@gmail.com <timek70@dalcomsoft.com>
2022-05-25 17:09:40 +0900

    Redux를 이용한 로그인 구현

---
+++ client/src/_actions/types.js
@@ -0,0 +1,2 @@
+// type들만 관리하는 파일
+export const LOGIN_USER = "login_user";
--- client/src/_reducer/index.js
+++ client/src/_reducer/index.js
@@ -1,10 +1,10 @@
 // 스토어에 Reducer가 여러가지 있을 수 있는데, Reducer는 어떻게 state가 변하는지를 보여주고, return한다.
 // CombineReducers를 이용해서 Root Reducer에서 하나로 합쳐줌
 import { combineReducers } from "redux";
-// import user from './user_reducer';
+import user from './user_reducer';
 
 const rootReducer = combineReducers({
-
+    user
 })
 
 export default rootReducer;
